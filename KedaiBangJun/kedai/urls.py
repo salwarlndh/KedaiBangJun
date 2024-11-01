bf96818d6489df5cbd6efb7c91b04c0cb8ef1ec6
Salwa Arlinda Humairah <126219178+salwarlndh@users.noreply.github.com>
2024-11-02 03:01:09 +0800

    Progress1

--- KedaiBangJun/kedai/urls.py
+++ KedaiBangJun/kedai/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about', views.about),
-    path('home', views.homepage),
+    path('home', views.homepage, name='home'),
+    path('about', views.about, name='about'),
+    path('product', views.product, name='product'),
+    path('contact', views.contact, name='contact'),
 ]
--- KedaiBangJun/kedai/views.py
+++ KedaiBangJun/kedai/views.py
@@ -5,3 +5,9 @@ def homepage(request):
 
 def about(request):
     return render(request, 'homepage/about.html')
+
+def product(request):
+    return render(request, 'homepage/product.html')
+
+def contact(request):
+    return render(request, 'homepage/contact.html')
